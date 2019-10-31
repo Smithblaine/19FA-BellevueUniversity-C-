31130a57199ea4f077f0a5ff44d4f588b22c6131
Blaine Smith <Ringospy@gmail.com>
2019-10-30 21:26:59 -0500

    Assignment1.4

---
+++ Assignment2/Assignment2/MoveEstimator.cs
@@ -0,0 +1,19 @@
+﻿//Blaine Smith
+//10-30-2019
+//Bellevue University
+//Assignment 1.2
+//This program makes use of writing strings out to the console
+
+using System;
+using static System.Console;
+
+namespace Assignment2
+{
+    class MoveEstimator
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
