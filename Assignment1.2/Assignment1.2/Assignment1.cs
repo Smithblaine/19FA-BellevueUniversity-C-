767940a2c5f744ffc7dfa859b0ac937456c47d65
Blaine Smith <Ringospy@gmail.com>
2019-10-30 19:37:30 -0500

    Assignment 1.2

---
+++ Assignment1.2/Assignment1.2/Assignment1.cs
@@ -0,0 +1,22 @@
+﻿//Blaine Smith
+//10-30-2019
+//Bellevue University
+//Assignment 1.2
+//This program makes use of writing strings out to the console
+
+using System;
+using static System.Console;
+
+namespace Assignment1._2
+{
+    class Assignment1
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Don't worry about a thing");
+            WriteLine("Cause every little thing gonna be all right");
+            WriteLine("Singin Don't worry about a thing");
+            WriteLine("Cause every little thing gonna be all right!\n");
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using static System.Console;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Hello World!");
+        }
+    }
+}
