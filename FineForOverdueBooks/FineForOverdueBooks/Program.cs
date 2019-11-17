751500413cbcea48d276ce770cbfaf70fcda7f91
Blaine Smith <Ringospy@gmail.com>
2019-11-17 00:01:15 -0600

    Assignment4.3 finish

--- FineForOverdueBooks/FineForOverdueBooks/Program.cs
+++ FineForOverdueBooks/FineForOverdueBooks/Program.cs
@@ -2,11 +2,13 @@
 //11-16-2019
 //Bellevue University
 //Assignment 4.3
+//This program will allow a user to enter a total number of books and days to receive the total fine for their past due books.
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
 
 namespace FineForOverdueBooks
 {
@@ -14,7 +16,36 @@ namespace FineForOverdueBooks
     {
         static void Main(string[] args)
         {
-            //hello world
+            double underSeven = .10;
+            double overSeven = .30;
+            bool numberOfBooks;
+            bool daysPast;
+
+            Write("Enter in the number of book you have: ");
+            int books;
+            numberOfBooks = int.TryParse(ReadLine(), out books);
+
+            Write("\nHow many days are these books past due: ");
+            int days;
+            daysPast = int.TryParse(ReadLine(), out days);
+
+            OverDueCost(books, days, underSeven, overSeven);
+        }
+
+
+        static void OverDueCost(int book, int day, double under, double over)
+        {
+            double totalUnder = book * (day * under);
+            double totalOver = book * (day * over) - totalUnder;
+
+            if (day <= 7)
+            {
+                WriteLine("The total fine for {0} book(s),\nFor {1} days at {2:C2} a day is {3:C2}.", book, day, under, totalUnder);
+            }
+            else if (day > 7)
+            {
+                WriteLine("The total fine for {0} book(s),\nFor {1} days at {2:C2} a day is {3:C2}.", book, day, over, totalOver);
+            }
         }
     }
 }
