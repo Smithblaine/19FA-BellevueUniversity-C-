57f368acb70197bc294a4bdad298cc8673bf8a01
Blaine Smith <Ringospy@gmail.com>
2019-11-16 19:10:55 -0600

    Assignment4.3

---
+++ FineForOverdueBooks/FineForOverdueBooks/Program.cs
@@ -0,0 +1,20 @@
+﻿//Blaine Smith
+//11-16-2019
+//Bellevue University
+//Assignment 4.3
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FineForOverdueBooks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //hello world
+        }
+    }
+}
